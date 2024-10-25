f37878b480f447d2f5a4940a58936a84e1cf7668
Alexander Green <alexander.green@logius.nl>
2024-10-25 10:50:19 +0200

    DK release candidate

---
+++ dk/restapi/rc202410/js/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Koppelvlakstandaard-REST-API.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Peter Haasnoot"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Pieter Hering"
+      }
+    ],
+  editors: [ {    
+        "company" : "Logius",  
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Standaardenbeheer"
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
+  nl_emailcomments: "digikoppeling@logius.nl",
+  previousPublishVersion: "1.1.1",
+  pubDomain: "dk",
+  publishDate: "2024-09-19",
+  publishVersion: "",
+  shortName: "restapi",
+  specStatus: "CV",
+  specType: "ST"
+};
