f46d981996421dab5d61c24321c72c5032145f12
Alexander Green <alexander.green@logius.nl>
2024-11-20 16:51:06 +0100

    Versie ter Vaststelling FSC verplicht

---
+++ dk/restapi/fsc202411/js/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Koppelvlakstandaard-REST-API.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Peter Haasnoot"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Pieter Hering"
+      }
+    ],
+  editors: [ {    
+        "company" : "Logius",  
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Standaardenbeheer"
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
+  nl_emailcomments: "digikoppeling@logius.nl",
+  previousPublishVersion: "1.1.0",
+  pubDomain: "dk",
+  publishDate: "2024-11-20",
+  publishVersion: "1.1.1",
+  shortName: "restapi",
+  specStatus: "VV",
+  specType: "ST"
+};
