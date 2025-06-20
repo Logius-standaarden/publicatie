5fbcdbedb0e7ed6fb5dc37e355d809234c944fb8
Nil Netra Barua <nil.barua@logius.nl>
2025-06-20 09:25:20 +0200

    Update organisation-config.js

--- respec/organisation-config.js
+++ respec/organisation-config.js
@@ -408,7 +408,7 @@ var organisationConfig = {
             publisher: "NCSC",
             title: "ICT-beveiligingsrichtlijnen voor Transport Layer Security (TLS) v2.1"
         },
-        "NCSC 2024": {
+        "NCSC 2025": {
             date: "June 2025",
             href: "https://www.ncsc.nl/wat-kun-je-zelf-doen/documenten/publicaties/2025/juni/01/ict-beveiligingsrichtlijnen-voor-transport-layer-security-2025-05",
             publisher: "NCSC",
